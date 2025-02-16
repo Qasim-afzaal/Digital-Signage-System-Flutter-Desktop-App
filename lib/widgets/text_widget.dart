054b5d4330383560df055c0a2d9bfade95bbe7bc
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-02-17 01:11:11 +0500

    add text widget

--- lib/widgets/text_widget.dart
+++ lib/widgets/text_widget.dart
@@ -4,21 +4,25 @@ class SimpleText extends StatelessWidget {
   final String text;
   final double fontSize;
   final FontWeight fontWeight;
+  final Color color;
+  final TextAlign textAlign;
 
   const SimpleText({
     required this.text,
     this.fontSize = 13.0,
     this.fontWeight = FontWeight.normal,
+    this.color = Colors.white,
+    this.textAlign = TextAlign.center,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      textAlign: TextAlign.center,
       text,
+      textAlign: textAlign, // Moved here inside the Text widget
       style: TextStyle(
-        color: Colors.white,
+        color: color,
         fontSize: fontSize,
         fontWeight: fontWeight,
       ),
